7c24541f8d9b0e9500a6cb8ed15490ce3f410610
soczystys <34406205+soczystys@users.noreply.github.com>
2023-02-13 20:38:42 +0100

    Dodanie klas Drink, Ingredient, oraz uzupełnienie metody main

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,6 +3,34 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
+        Drink drink1 = new Drink();
+
+        drink1.name = "Mojito";
+        drink1.cost = 13;
+        drink1.isWithAlcohol = true;
+
+        drink1.ingredient1 = new Ingredient();
+        drink1.ingredient2 = new Ingredient();
+        drink1.ingredient3 = new Ingredient();
+
+        drink1.ingredient1.name = "woda";
+        drink1.ingredient1.volume = 160;
+
+        drink1.ingredient2.name = "sok cytrynowy";
+        drink1.ingredient2.volume = 10;
+
+        drink1.ingredient3.name = "wódka";
+        drink1.ingredient3.volume = 40;
+
+        System.out.println("Drink: " + drink1.name);
+        System.out.println("Cena: " + drink1.cost + "zł");
+        System.out.println("Czy zawiera alkohol? " + drink1.isWithAlcohol);
+        System.out.println("Składniki:");
+        System.out.println(drink1.ingredient1.name);
+        System.out.println(drink1.ingredient2.name);
+        System.out.println(drink1.ingredient3.name);
+        System.out.println("Pojemność:");
+        System.out.println((drink1.ingredient1.volume + drink1.ingredient2.volume + drink1.ingredient3.volume) + "ml");
 
     }
 }
